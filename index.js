fc4f2c2f231274025921ea08d02eef5bc55410bd
Bahareh <mer_ir@yahoo.com>
2023-04-29 16:17:20 -0700

    add the query of add a department

--- index.js
+++ index.js
@@ -11,16 +11,7 @@ const db = mySql.createConnection({
 console.log(`Connected to the employees_db database.`)
 );
     
-// [
-//     "view all departments",
-//     "view all roles",
-//     "view all employees",
-//     "add a department",
-//     "add a role",
-//     "add an employee",
-//     "update an employee's role",
-//     "quit"
-// ]    
+  
 function askQuestion() {      
     inquirer
         .prompt({
@@ -30,7 +21,8 @@ function askQuestion() {
             choices: [
                 "view all departments",
                 "view all roles",
-                "view all employees","add a department",
+                "view all employees",
+                "add a department",
                 "add a role",
                 "add an employee",
                 "update an employee's role",
@@ -46,6 +38,12 @@ function askQuestion() {
                 case "view all roles":
                     showAllRoles();
                     break;
+                case "view all employees":
+                    showAllEmployees();
+                    break;
+                case "add a department":
+                    addADepartment();
+                    break;
                 case "quit":
                     db.end();
             }
@@ -58,7 +56,7 @@ function askQuestion() {
 }
 
 function showAllDepartments(){
-    const queryTxt = 'select * from department';
+    const queryTxt = 'SELECT * FROM department';
     db.promise().query(queryTxt)
         .then(([rows, fields]) =>{
             console.table('department',rows);
@@ -70,10 +68,10 @@ function showAllDepartments(){
 }
 
 function showAllRoles(){
-    const queryTxt = `select role.id, role.title, department.name as department_name, role.salary
-                   from role
-                   inner join department 
-                   on role.department_id = department.id`;
+    const queryTxt = `SELECT role.id, role.title, department.name as department_name, role.salary
+                   FROM role
+                   INNER JOIN department 
+                   ON role.department_id = department.id`;
     db.promise().query(queryTxt)
         .then(([rows, fields]) =>{
             console.table('role', rows);
@@ -83,6 +81,56 @@ function showAllRoles(){
             console.log(err);
         })
 }
+
+function showAllEmployees(){
+    const queryTxt = `SELECT employee.first_name, employee.last_name, role.title, 
+                      concat(manager.first_name, ' ', manager.last_name) AS manager_name
+                      FROM employee AS employee
+                      INNER JOIN role ON employee.role_id = role.id
+                      lEFT JOIN employee AS manager ON employee.id = manager.id`;
+    db.promise().query(queryTxt)
+        .then(([rows, fields]) =>{
+            console.table('employee', rows);
+            askQuestion();
+        })
+        .catch(err=>{
+            console.log(err);
+        })
+}
+//------------------------------------------------ Add a Department ----------------------------------------------------
+function addADepartment(){
+    const departmentQuestion = [{
+        type: "input",
+        message: "What is the name of new department? (write quit if you have given up) ",
+        name: "departmentName",
+    }]
+    inquirer
+        .prompt(departmentQuestion)
+        .then((answer) => {
+            if(answer === "quit"){
+                askQuestion();
+                return; 
+            }
+            let  {departmentName}= answer;
+            const queryTxt = `INSERT INTO department(name)
+                              VALUES (?)`;
+            db.promise().query(queryTxt, [departmentName])
+                .then(([rows, fields]) =>{
+                    console.log(`New department of ${departmentName} is added.`)
+                    askQuestion();
+                })
+                .catch(err=>{
+                    console.log(err);
+                })
+        })
+        .catch((error) => {
+            console.log(error);
+            console.log("Sorry! Something went wrong!")
+        })
+}
+
+//------------------------------------------------ Add a Role ----------------------------------------------------
+
 
 
 askQuestion();
